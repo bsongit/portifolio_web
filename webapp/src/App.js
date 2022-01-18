a21a3b035c530955e4e48b8fd550c8ba9d1f3a85
besonquadros@gmail.com <besonquadros@gmail.com>
2022-01-18 00:56:19 -0300

    Adding webapp folder

---
+++ webapp/src/App.js
@@ -0,0 +1,6 @@
+import Button from './components/Button'
+function App() {
+  return (<Button></Button>);
+}
+
+export default App;
---
+++ webapp/src/components/Button.js
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+import '../assets/styles/button.css'
+
+class Button extends Component{
+    render(){
+        return <button className="btn btn-success">Werbeson</button>
+    }
+}
+
+export default Button;
